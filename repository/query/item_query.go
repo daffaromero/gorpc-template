1c9bb50a22fe951204219a69da6c8e2ae186b672
Daffa Muhammad Romero <daffaromero@mail.ugm.ac.id>
2024-09-04 14:35:54 +0700

    Quick and dirty item query

--- repository/query/item_query.go
+++ repository/query/item_query.go
@@ -1,2 +1,94 @@
 package query
 
+import (
+	"context"
+	"gorpc-template/v2/protobuf/api"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type ItemQuery interface {
+	CreateItem(ctx context.Context, tx pgx.Tx, item *api.Item) error
+	GetItem(ctx context.Context, id string) (*api.Item, error)
+	ListItems(ctx context.Context) ([]*api.Item, error)
+	UpdateItem(ctx context.Context, tx pgx.Tx, item *api.Item) error
+	DeleteItem(ctx context.Context, tx pgx.Tx, id string) error
+}
+
+type itemQuery struct {
+	db *pgxpool.Pool
+}
+
+func NewItemQuery(db *pgxpool.Pool) *itemQuery {
+	return &itemQuery{db: db}
+}
+
+func (q *itemQuery) CreateItem(ctx context.Context, tx pgx.Tx, item *api.Item) error {
+	query := `INSERT INTO items (id, name, description) VALUES ($1, $2, $3)`
+
+	_, err := tx.Exec(ctx, query, item.Id, item.Name, item.Description)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (q *itemQuery) GetItem(ctx context.Context, id string) (*api.Item, error) {
+	query := `SELECT id, name, description FROM items WHERE id = $1`
+
+	row := q.db.QueryRow(ctx, query, id)
+	var item api.Item
+
+	err := row.Scan(&item.Id, &item.Name, &item.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	return &item, nil
+}
+
+func (q *itemQuery) ListItems(ctx context.Context) ([]*api.Item, error) {
+	query := `SELECT id, name, description FROM items`
+
+	rows, err := q.db.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	var items []*api.Item
+
+	for rows.Next() {
+		var item api.Item
+		err := rows.Scan(&item.Id, &item.Name, &item.Description)
+		if err != nil {
+			return nil, err
+		}
+		items = append(items, &item)
+	}
+	return items, nil
+}
+
+func (q *itemQuery) UpdateItem(ctx context.Context, tx pgx.Tx, item *api.Item) error {
+	query := `UPDATE items SET name = $1, description = $2 WHERE id = $3`
+
+	_, err := tx.Exec(ctx, query, item.Name, item.Description, item.Id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (q *itemQuery) DeleteItem(ctx context.Context, tx pgx.Tx, id string) error {
+	query := `DELETE FROM items WHERE id = $1`
+
+	_, err := tx.Exec(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
